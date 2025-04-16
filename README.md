d5cdbff634ab2c13976e50d3f45bf4c885d84552
yeyuqh <zhaoxw1028@gmail.com>
2025-04-16 12:26:21 +0800

    chore: init

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # uni-transition-bug-reproduction
+
+## 启动项目
+
+```bash
+npm i # 安装依赖
+npm run dev:mp-weixin # 运行微信小程序项目
+```
+
+## 导入项目到微信开发者工具
+
+1. 打开微信开发者工具
+2. 选择「导入」
+3. 选择 `uni-transition-bug-reproduction/dist/dev/mp-weixin`
---
+++ src/pages/index/index.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+
+const show = ref(false)
+</script>
+
+<template>
+  <view>
+    <view style="display: flex; justify-content: center; align-items: center; width: 100vw; height: 100vh;">
+      <button style="width: 100px;" @click="show = !show">SHOW</button>
+    </view>
+
+    <uni-transition
+    :show="show" mode-class="fade" :duration="1000"
+      :styles="{
+        position: 'fixed',
+        left: 0,
+        right: 0,
+        bottom: 0,
+        zIndex: 900,
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+      }"
+    >
+      <view style="display: flex; justify-content: center; align-items: center; width: 100vw; height: 100vh;">
+        <button style="width: 100px; background-color: orange;" @click="show = !show">HIDE</button>
+      </view>
+    </uni-transition>
+  </view>
+</template>
